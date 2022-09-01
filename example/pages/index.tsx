1ad97dcbf4fc1b3c975523d44051b756785684d0
khubo(aswin) <aswinrajv@gmail.com>
2022-09-01 11:24:01 +0530

    fix: fix tsup configuration

---
+++ example/pages/index.tsx
@@ -0,0 +1,39 @@
+import type { NextPage } from 'next'
+import { tasty } from 'tastycss'
+import styles from '../styles/Home.module.css'
+
+const Element = tasty({
+  as: 'span',
+  styles: {
+    '@local-padding': ['2x', '1x'], // responsive styles
+    '@text-color': 'rgba(255, 0, 0)',
+    padding: '@local-padding',
+    color: {
+      '': '#text',
+      blue: 'blue',
+    }, 
+  },
+  role: 'article',
+  styleProps: ['align'],
+});
+
+const Heading = tasty({
+  as: 'h1',
+  styles: {
+  styleProps: ['align'],
+  }
+});
+
+const Span = tasty('span', {
+});
+
+const Home: NextPage = () => {
+  return (
+    <div >
+    <Heading> TastyCSS</Heading>
+    <Element> yum!</Element>
+    </div>
+  )
+}
+
+export default Home
