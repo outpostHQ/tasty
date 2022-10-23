08cecef33eabd031841a8b2d0090a9fcab52eecf
shubham-kaushal <nsk9195@gmail.com>
2022-10-23 12:18:57 +0530

    fix: tyings and upgrade deps

--- packages/tastycss/tsup.config.ts
+++ packages/tastycss/tsup.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from 'tsup';
 import { findUpSync } from 'find-up';
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
-  dts: true,
   clean: true,
-  format: ['cjs', 'esm'],
+  format: ["cjs", "esm"],                                               
   inject: process.env.JSX ? [findUpSync('react-shim.js')!] : undefined,
-  external: ['react'],
-  treeshake: true,
-});
+})
