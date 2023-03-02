8a1ce4586b6a6ea3a2ea3a20d386aa010ec0132e
shubham-kaushal <nsk9195@gmail.com>
2023-03-02 23:35:17 +0530

    Variants API, camelCase support for mods, nested mods support.

---
+++ tsup.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "tsup"
+import { findUpSync } from "find-up"
+
+export default defineConfig({
+  clean: true,
+  format: ["cjs", "esm"],
+  inject: process.env.JSX ? [findUpSync("react-shim.js")!] : undefined,
+})
